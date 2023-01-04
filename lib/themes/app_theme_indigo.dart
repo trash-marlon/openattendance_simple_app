cf204a9b0759466976731721584d04ffdc71bbb3
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2023-01-04 22:09:48 +0100

    Add Theme

---
+++ lib/themes/app_theme_indigo.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class AppThemeIndigo {
+  static const Color primaryColor = Colors.indigo;
+
+  static final ThemeData lightTheme = ThemeData.light().copyWith(
+        // Color primario
+        primaryColor: primaryColor,
+
+        // AppBar
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          color: primaryColor,
+        ),
+
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            primary: primaryColor,
+          ),
+        ),
+
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: primaryColor,
+          elevation: 0
+        ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+           style: ElevatedButton.styleFrom(
+              primary: AppThemeIndigo.primaryColor,
+              shape: RoundedRectangleBorder(
+               borderRadius: BorderRadius.circular(20)
+              )
+           ),
+        ),
+      
+      inputDecorationTheme: const InputDecorationTheme(
+         floatingLabelStyle:  TextStyle(color: primaryColor),
+          labelStyle: TextStyle(color: primaryColor),
+
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: primaryColor),
+            borderRadius: BorderRadius.all(Radius.circular(10))
+          ),
+
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: primaryColor),
+            borderRadius: BorderRadius.all(Radius.circular(10))
+          ),
+
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: primaryColor),
+            borderRadius: BorderRadius.all(Radius.circular(10))
+          ),
+
+      ),
+
+      
+      );
+
+
+  static final ThemeData darkTheme = ThemeData.dark().copyWith(
+        // Color primario
+        primaryColor: primaryColor,
+
+        // AppBar
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          color: primaryColor,
+        ),
+
+        // Texto
+        scaffoldBackgroundColor: Colors.black
+      );
+}
